7d7c417e4ca6a1d476f9c2a1b20afffbd08c7d4e
xiejiangzhi <xiejiangzhi@gmail.com>
2024-02-27 11:28:01 +0800

    Update luax_tovector support table

--- src/api/l_math.c
+++ src/api/l_math.c
@@ -52,26 +52,44 @@ static void luax_destroypool(void) {
 }
 
 float* luax_tovector(lua_State* L, int index, VectorType* type) {
-  void* p = lua_touserdata(L, index);
+  if (lua_istable(L, index)) {
+    VectorType vtype;
+    switch(luax_len(L, index)) {
+    case 2: vtype = V_VEC2; break;
+    case 3: vtype = V_VEC3; break;
+    case 4: vtype = V_VEC4; break;
+    case 5: vtype = V_QUAT; break; // bad way to detect quat
+    case 16: vtype = V_MAT4; break;
+    default:
+      if (type) *type = V_NONE;
+      return NULL;
+    }
+    float* p;
+    lovrPoolAllocate(pool, vtype, &p);
+    luax_readobjarr(L, index, lovrVectorInfo[vtype].components, p, lovrVectorInfo[vtype].name);
+    if (type) *type = vtype;
+    return p;
+  } else {
+    void* p = lua_touserdata(L, index);
 
-  if (p) {
-    if (lua_type(L, index) == LUA_TLIGHTUSERDATA) {
-      Vector v = { .pointer = p };
-      if (v.handle.type > V_NONE && v.handle.type < MAX_VECTOR_TYPES) {
-        if (type) *type = v.handle.type;
-        return lovrPoolResolve(pool, v);
-      }
-    } else {
-      VectorType* t = p;
-      if (*t > V_NONE && *t < MAX_VECTOR_TYPES) {
-        if (type) *type = *t;
-        return (float*) (t + 1);
+    if (p) {
+      if (lua_type(L, index) == LUA_TLIGHTUSERDATA) {
+        Vector v = { .pointer = p };
+        if (v.handle.type > V_NONE && v.handle.type < MAX_VECTOR_TYPES) {
+          if (type) *type = v.handle.type;
+          return lovrPoolResolve(pool, v);
+        }
+      } else {
+        VectorType* t = p;
+        if (*t > V_NONE && *t < MAX_VECTOR_TYPES) {
+          if (type) *type = *t;
+          return (float*) (t + 1);
+        }
       }
     }
+    if (type) *type = V_NONE;
+    return NULL;
   }
-
-  if (type) *type = V_NONE;
-  return NULL;
 }
 
 float* luax_checkvector(lua_State* L, int index, VectorType type, const char* expected) {
