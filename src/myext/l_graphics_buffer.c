86ade9f7c1ea082ebb782c1115a862df6cd7265c
xiejiangzhi <xiejiangzhi@gmail.com>
2024-06-23 16:28:12 +0800

    Fix Buffer:setDataEx

--- src/myext/l_graphics_buffer.c
+++ src/myext/l_graphics_buffer.c
@@ -11,8 +11,12 @@ static void luax_checkfield(lua_State* L, const DataField* field, int idx, char*
     luax_checkfieldv(L, idx, field, data);
     break;
   case LUA_TTABLE:
-    lovrCheck(luax_len(L, idx) == n, "Table length for key '%s' must be %d", field->name, n);
-    luax_checkfieldt(L, idx, field, data + field->offset);
+    int len = luax_len(L, idx);
+    lovrCheck(
+      len == n, "Table length for key '%s#%s' must be %d, got %i",
+      field->parent ? field->parent->name : "Root", field->name, n, len
+    );
+    luax_checkfieldt(L, idx, field, data);
     break;
   default:
     lovrThrow("Invalid data type %s for field '%s'", lua_typename(L, lua_type(L, idx)), field->name);
@@ -22,43 +26,57 @@ static void luax_checkfield(lua_State* L, const DataField* field, int idx, char*
 static void luax_checkfields(lua_State* L, const DataField* format, int tidx, char* data) {
   if (tidx < 0) tidx = lua_gettop(L) + tidx + 1;
 
-  lovrCheck(
-    luax_len(L, tidx) == format->fieldCount,
-    "Expect %s has %i elements, but got %i",
-    format->name, format->fieldCount, luax_len(L, tidx)
-  );
+  int ttype = lua_type(L, tidx);
+  int tlen = ttype == LUA_TTABLE ? luax_len(L, tidx) : -1;
 
-  for (int i = 0; i < format->fieldCount; i++) {
-    lua_rawgeti(L, tidx, i + 1);
-    DataField* field = format->fields + i;
-    char* fdata = data + field->offset;
+  if (format->length > 0 && (format->parent || format->fieldCount == 0)) {
+    lovrCheck(
+      lua_istable(L, tidx),
+      "Array %s val must be a table", format->name ? format->name : "None"
+    );
 
-    if (field->fieldCount == 0) {
-      if (field->length == 0) {
-        luax_checkfield(L, field, -1, fdata);
-      } else {
-        lovrCheck(lua_istable(L, -1), "field %s val must be a table");
-        int len = MIN(luax_len(L, -1), field->length);
-        for (int j = 0; j < len; j++) {
-          lua_rawgeti(L, -1, j + 1);
-          luax_checkfield(L, field, -1, fdata + j * field->stride);
-          lua_pop(L, 1);
-        }
-      }
-    } else {
-      if (field->length == 0) {
+    int len = MIN(luax_len(L, -1), format->length);
+    for (int i = 0; i < len; i++) {
+      lua_rawgeti(L, tidx, i + 1);
+
+      if (format->fieldCount == 0) {
+        DataField* field = format->fields ? format->fields : format;
+        char* fdata = data + field->stride * i;
         luax_checkfields(L, field, -1, fdata);
       } else {
-        lovrCheck(lua_istable(L, -1), "field %s val must be a table");
-        int len = MIN(luax_len(L, -1), field->length);
-        for (int j = 0; j < len; j++) {
+        for (int j = 0; j < format->fieldCount; j++) {
+          DataField* field = format->fields + j;
+          char* fdata = data + field->stride * i + field->offset;
           lua_rawgeti(L, -1, j + 1);
-          luax_checkfields(L, field, -1, fdata + j * field->stride);
+          luax_checkfields(L, field, -1, fdata);
           lua_pop(L, 1);
         }
       }
+      lua_pop(L, 1);
     }
-    lua_pop(L, 1);
+    return;
+  } else if (format->fieldCount > 0) {
+    lovrCheck(
+      lua_istable(L, tidx), "Struct %s val must be a table",
+      format->name ? format->name : "None"
+    );
+    lovrCheck(
+      luax_len(L, tidx) == format->fieldCount,
+      "Expect %s has %i fields, but got %i",
+      format->name, format->fieldCount, luax_len(L, tidx)
+    );
+
+    for (int i = 0; i < format->fieldCount; i++) {
+      lua_rawgeti(L, tidx, i + 1);
+      DataField* field = format->fields + i;
+      char* fdata = data + field->offset;
+
+      luax_checkfields(L, field, -1, fdata);
+      lua_pop(L, 1);
+    }
+  } else {
+    luax_checkfield(L, format, tidx, data);
+    return;
   }
 }
 
@@ -68,11 +86,12 @@ static int l_lovrBufferSetDataEx(lua_State* L) {
   const BufferInfo* info = lovrBufferGetInfo(buffer);
   const DataField* format = info->format;
   lovrCheck(format, "Buffer must be created with format information to copy a table to it");
-  lovrCheck(
-    luax_len(L, 2) == format->fieldCount,
-    "Expect buffer has %i elements, but got %i",
-    format->fieldCount, luax_len(L, 2)
-  );
+  // int len = luax_len(L, 2);
+  // lovrCheck(
+  //   len == format->fieldCount || len == format->length,
+  //   "Expect buffer has %i elements, but got %i",
+  //   format->fieldCount, luax_len(L, 2)
+  // );
   luax_checkfields(L, format, 2, lovrBufferSetData(buffer, 0, ~0u));
   return 0;
 }
